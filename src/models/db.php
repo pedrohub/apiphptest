f270a55030f56aa300636f3c5f40735b4dfab3a2
Pedro Edson <pedroedson.lima@gmail.com>
2018-04-02 01:58:04 -0300

    adicionado instruções de acesso ao banco mysql

--- src/models/db.php
+++ src/models/db.php
@@ -6,10 +6,10 @@
  * @author pedro.edson.o.lima
  */
 class db {
-	private $dbhost = 'localhost';
-	private $dbuser = 'user';
-	private $dbpass = 'pass';
-	private $dbname = 'database';
+	private $dbhost = 'localhost';//ajustar para seu host
+	private $dbuser = 'user';// ajustar para usuario do seu banco mysql
+	private $dbpass = 'pass';// informar senha do user
+	private $dbname = 'database'; // trocar para o database criado no arquivo db.sql 
 
 	public function connect() {
 		$mysql_connect_str = "mysql:host=$this->dbhost;dbname=$this->dbname";
