272615790cbc2a629941a64b175ffa93b77d8d01
pedrohub <pedroedson.lima@gmail.com>
2018-04-02 01:47:20 -0300

    balanced brackets

--- src/controllers/BracketController.php
+++ src/controllers/BracketController.php
@@ -7,23 +7,76 @@
  */
 class BracketController {
 
+	/**
+	 * Balanced brackets
+	 * @param String $value
+	 * @return string
+	 */
 	public function validateStringBracket($value) {
 
-		if(preg_match('[\(\)\[\]\{\}]', $value)){
-			return "entrou";
+		$nok = "$value fora do padr&atilde;o";
+		$ok = "$value est&aacute; no padr&atilde;o correto";
+		
+		if(preg_match('/[\(\)\[\]\{\}]/', $value)){
+		
+			if(!$this->validateSize($value)){
+				return $nok;
+			}
+		
+			//converter para validar o caracteres
+			$arrayValue = str_split($value);
+			$count = count($arrayValue)-1;
+		
+			if ($arrayValue[$count] == "{" || $arrayValue[$count] == "[" || $arrayValue[$count] == "("){
+				return $nok;
+			}
+				
+			if ($arrayValue[0] == "]" || $arrayValue[0] == "}" || $arrayValue[0] == ")"){
+				return $nok;
+			}	
+			
+			$caracteresClose = array("}", ")", "]");
+			
+			$countMatch = 0;
+			for ($i = 0; $i < $count; $i++) {
+				
+				if (($arrayValue[$i] < $count) && (!in_array($arrayValue[$i], $caracteresClose))){
+					$countMatch = 0;
+					if(($arrayValue[$i] == "[") && ($arrayValue[$i+1] == "]" || $arrayValue[$i+1] == "{")) {
+						$countMatch++;
+					} else if((($arrayValue[$i] == "(") && ($arrayValue[$i+1] == ")"))) {
+						$countMatch++;
+					} else if(($arrayValue[$i] == "{") && ($arrayValue[$i+1] == "}" || $arrayValue[$i+1] == "(")) {
+						$countMatch++;
+					}
+				}
+ 			}
+ 			
+ 			if ($countMatch == 0) {
+ 				return $nok;
+ 			}
+			
+			return $ok;
 		}
 		
-		return $value." agora";
+		return $nok;
 	}
 	
-	private function validateBrackets($value){
+	/**
+	 * Validar match dos caracteres
+	 * @param Char[] $value
+	 * @return boolean
+	 */
+	private function validateSize($value){
 		
+		if((substr_count($value , '(') == substr_count($value , ')'))
+				&& (substr_count($value , '[') == substr_count($value , ']'))
+				&& (substr_count($value , '{') == substr_count($value , '}'))){
+			return true;
+		}
 		
+		return false;
 		
 	}
 	
-	private function validate($value){
-		
-		
-	}
 }	
